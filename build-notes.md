883329260a4e7a8940d3d8df64ceb8b1ab530b8e
jefftadashi <jeff@jefftadashi.com>
2020-06-07 21:28:39 -0500

    module update structure

---
+++ build-notes.md
@@ -0,0 +1,10 @@
+
+# Internal Notes
+
+To build:
+ - Update version in setup.py
+ - run `python3 setup.py sdist bdist_wheel`
+ - run `twine upload dist/*`
+ - delete build folder when done
+
+See references here: https://packaging.python.org/tutorials/packaging-projects/
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ping-overseer",
+    version="0.0.1",
+    author="Jeff Tadashi Moy",
+    author_email="jeff@jefftadashi.com",
+    description="Mass ping module!",
+    install_requires=[            
+          'nmap',
+          'jefftadashi_utils',
+    ],
+    license="gpl-3.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JeffTadashi/ping-overseer",
+    packages=setuptools.find_packages(),
+    scripts=['ping-overseer/ping-overseer'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
